86e0e23e6f6c64f05c87f056ba1b4b6fb7748ea5
soulwine-it <skyorwind@gmail.com>
2019-12-09 20:20:32 +0900

    update URL pattern

--- Scope/src/sec01/ex01/SetAttribute.java
+++ Scope/src/sec01/ex01/SetAttribute.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/set")
+//@WebServlet("/set")
 public class SetAttribute extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 		response.setContentType("text/html;charset=utf-8");
---
+++ Scope/src/sec02/ex01/TestServlet3.java
@@ -0,0 +1,37 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do") //확장자만 일치하는 패턴
+//@WebServlet("/*") 모든 요청 URL패턴
+
+public class TestServlet3 extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		String context = request.getContextPath(); //컨텍스트의 이름만 가져옵니다.
+		String url = request.getRequestURL().toString(); //전체 URL을 가져옵니다.
+		String mapping = request.getServletPath(); // 서블릿 매핑 이름만 가져옵니다.
+		String uri = request.getRequestURI(); //URI를 가져옵니다.
+		
+		out.println("<html><head>><title>Test Servlet1</title></head>");
+		out.println("<body bgcolor='green'>");
+		out.println("<b>TestServlet3입니다.</b><br>");
+		out.println("<b>컨텍스트 이름 :" + context + "</b><br>");
+		out.println("<b>전체 경로:"+ url +"</b><br>");
+		out.println("<b>매핑 이름 : " + mapping+ "</b><br>");
+		out.println("<b>URI : " + uri + "<b>");
+		out.println("</body></html>");
+		out.close();
+	}
+
+}
